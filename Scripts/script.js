ce3183455e2bf64bc2809358590feff84ff154da
Alireza <126352578+Alireza-Jodat2001@users.noreply.github.com>
2024-03-03 00:09:07 +0330

    Start fetching

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,3 +4,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+request.open('Get', 'https://restcountries.com/v3.1/all');
+request.send();
+request.addEventListener('load', function () {
+    const request = JSON.parse(this.responseText);
+});
