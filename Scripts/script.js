e75d282f3ecd5ce045bc38af30f8a1934799bb81
Alireza <126352578+Alireza-Jodat2001@users.noreply.github.com>
2024-03-03 23:51:15 +0330

    Building the country element

--- Scripts/script.js
+++ Scripts/script.js
@@ -10,4 +10,28 @@ request.open('Get', 'https://restcountries.com/v3.1/all');
 request.send();
 request.addEventListener('load', function () {
     const request = JSON.parse(this.responseText);
+    console.log(request);
+    request.forEach(country => {
+        const { flag, name, region, population, languages, currencies } =
+            country;
+        const countryEl = `
+            <article class="country">
+                <img class="country__img" src="${flag}" />
+                <div class="country__data">
+                    <h3 class="country__name">${name.common}</h3>
+                    <h4 class="country__region">${region}</h4>
+                    <p class="country__row">
+                        <span>👫</span>${(+population / 1_000_000).toFixed(
+                            1
+                        )} people
+                    </p>
+                    <p class="country__row">
+                        <span>🗣️</span>${languages.name}
+                    </p>
+                    <p class="country__row">
+                        <span>💰</span>${currencies.name}
+                    </p>
+                </div>
+            </article>`;
+    });
 });
