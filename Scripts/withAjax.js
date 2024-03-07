c210630e7cb7c6e40fd90f578a5b35b0f93ff1bd
Alireza <126352578+Alireza-Jodat2001@users.noreply.github.com>
2024-03-07 22:48:55 +0330

    rewriting again the code with Fetch -D

--- Scripts/withAjax.js
+++ Scripts/withAjax.js
@@ -7,12 +7,15 @@ const btn = document.querySelector('.btn-country'),
     searchContainer = document.querySelector('.search-container'),
     nav = document.querySelector('.navbar'),
     searchPreset = document.querySelector('.search-preset'),
-    backdrop = document.querySelector('.backdrop');
+    backdrop = document.querySelector('.backdrop'),
+    checkboxInput = document.querySelector('.checkbox-input');
+
+let checked = false;
 
 ///////////////////////////////////////
 
 // Create Request
-function createRequest(search) {
+function createRequest(search, borderChecked) {
     const request = new XMLHttpRequest();
     // route URL
     const routeURL = search === 'all' ? 'all' : `name/${search}`;
@@ -20,13 +23,37 @@ function createRequest(search) {
     request.send();
     request.addEventListener('load', function () {
         const requestJSON = JSON.parse(this.responseText);
-        requestJSON.forEach(country => extractingData(country));
+        requestJSON.forEach(country => {
+            extractingData(country);
+        });
+        // create request for border countries
+        if (borderChecked) borderRequest(requestJSON);
     });
 }
-// createRequest('all');
+// createRequest('all', false);
+
+// create request for border countries
+function borderRequest(requestJSON) {
+    requestJSON.forEach(country => {
+        country.borders.forEach(border => {
+            const request = new XMLHttpRequest();
+            request.open(
+                'Get',
+                `https://restcountries.com/v3.1/alpha/${border}`
+            );
+            request.send();
+            request.addEventListener('load', function () {
+                const requestJSON = JSON.parse(this.responseText);
+                requestJSON.forEach(country =>
+                    extractingData(country, 'neighbour')
+                );
+            });
+        });
+    });
+}
 
 // extracting data of fetch
-function extractingData(country) {
+function extractingData(country, className) {
     const { flags, name, region, population, languages, currencies } = country,
         popM = +population / 1_000_000,
         popCalced = popM < 0.099 ? popM.toFixed(2) : popM.toFixed(1);
@@ -34,17 +61,33 @@ function extractingData(country) {
     for (const key in currencies) currName = currencies[key].name;
     for (const key in languages) langName = languages[key];
     // create arguments
-    const argument = { flags, name, region, popCalced, currName, langName };
+    const argument = {
+        flags,
+        name,
+        region,
+        popCalced,
+        currName,
+        langName,
+        className,
+    };
     // Create Card
     createCardEl(argument);
 }
 
 // Create card elements
 function createCardEl(argument) {
-    const { flags, name, region, popCalced, currName, langName } = argument;
+    const {
+        flags,
+        name,
+        region,
+        popCalced,
+        currName,
+        langName,
+        className = '',
+    } = argument;
     // Create Element
     const countryEl = `
-        <article class="country">
+        <article class="country ${className}">
             <img class="country__img" src="${flags.svg}" />
             <div class="country__data">
                 <h3 class="country__name">${name.common}</h3>
@@ -65,12 +108,17 @@ function createCardEl(argument) {
     countriesContainer.style.opacity = 1;
 }
 
+// checkbox event
+checkboxInput.addEventListener('change', function () {
+    checked = this.checked;
+});
+
 // form submited
 form.addEventListener('submit', e => {
     e.preventDefault();
     countriesContainer.textContent = '';
     // Create Request
-    createRequest(searchInput.value);
+    createRequest(searchInput.value, checked);
 });
 
 // Element Class
@@ -87,6 +135,7 @@ class Element {
         this.#element.classList.replace('show', 'hide');
     }
 }
+
 // new Element instance of class
 const searchPresetC = new Element(searchPreset),
     backdropC = new Element(backdrop);
---
+++ Scripts/withFetch.js
@@ -0,0 +1,146 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country'),
+    countriesContainer = document.querySelector('.countries'),
+    form = document.querySelector('.search-field'),
+    searchInput = form.querySelector('.search-input'),
+    searchContainer = document.querySelector('.search-container'),
+    nav = document.querySelector('.navbar'),
+    searchPreset = document.querySelector('.search-preset'),
+    backdrop = document.querySelector('.backdrop'),
+    checkboxInput = document.querySelector('.checkbox-input');
+
+let checked = false;
+
+// Element Class
+class Element {
+    #element;
+    constructor(element) {
+        this.#element = element;
+    }
+    // show search preset
+    _showEl() {
+        this.#element.classList.replace('hide', 'show');
+    }
+    _hideEl() {
+        this.#element.classList.replace('show', 'hide');
+    }
+}
+// new Element instance of class
+const searchPresetC = new Element(searchPreset),
+    backdropC = new Element(backdrop);
+
+///////////////////////////////////////
+
+// Create Request
+function createRequest(search, borderChecked) {
+    const routeURL = search === 'all' ? 'all' : `name/${search}`;
+    fetch(`https://restcountries.com/v3.1/${routeURL}`)
+        .then(response => response.json())
+        .then(requestJSON => {
+            requestJSON.forEach(country => {
+                extractingData(country);
+            });
+            // create request for border countries
+            if (borderChecked) borderRequest(requestJSON);
+        });
+}
+createRequest('all', false);
+
+// form submited
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    countriesContainer.textContent = '';
+    // Create Request
+    createRequest(searchInput.value, checked);
+});
+
+// checkbox event
+checkboxInput.addEventListener('change', function () {
+    checked = this.checked;
+});
+
+// extracting data of fetch
+function extractingData(country, className) {
+    const { flags, name, region, population, languages, currencies } = country,
+        popM = +population / 1_000_000,
+        popCalced = popM < 0.099 ? popM.toFixed(2) : popM.toFixed(1);
+    let currName, langName;
+    for (const key in currencies) currName = currencies[key].name;
+    for (const key in languages) langName = languages[key];
+    // create arguments
+    const argument = {
+        flags,
+        name,
+        region,
+        popCalced,
+        currName,
+        langName,
+        className,
+    };
+    // Create Card
+    createCardEl(argument);
+}
+
+// create request for border countries
+function borderRequest(requestJSON) {
+    requestJSON.forEach(country => {
+        country.borders.forEach(border => {
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                .then(response => response.json())
+                .then(requestJSON => {
+                    requestJSON.forEach(country =>
+                        extractingData(country, 'neighbour')
+                    );
+                });
+        });
+    });
+}
+
+// Create card elements
+function createCardEl(argument) {
+    const {
+        flags,
+        name,
+        region,
+        popCalced,
+        currName,
+        langName,
+        className = '',
+    } = argument;
+    // Create Element
+    const countryEl = `
+        <article class="country ${className}">
+            <img class="country__img" src="${flags.svg}" />
+            <div class="country__data">
+                <h3 class="country__name">${name.common}</h3>
+                <h4 class="country__region">${region}</h4>
+                <p class="country__row">
+                    <span>👫</span>${popCalced} people
+                </p>
+                <p class="country__row">
+                    <span>🗣️</span>${langName}
+                </p>
+                <p class="country__row">
+                    <span>💰</span>${currName}
+                </p>
+            </div>
+        </article>`;
+    // Insert Element
+    countriesContainer.insertAdjacentHTML('beforeend', countryEl);
+    countriesContainer.style.opacity = 1;
+}
+
+// navbar click event
+nav.addEventListener('click', e => {
+    const { target } = e;
+    if (target.closest('.search-container'))
+        [searchPresetC, backdropC].forEach(el => el._showEl());
+    else if (target.closest('.navbar'))
+        [searchPresetC, backdropC].forEach(el => el._hideEl());
+});
+
+// backdrop click event
+backdrop.addEventListener('click', function () {
+    [searchPresetC, backdropC].forEach(el => el._hideEl());
+});
